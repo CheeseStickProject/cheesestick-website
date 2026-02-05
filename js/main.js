1403e96da1d74c00c430404ba6f2d1f36a392282
theyeetbear <brodyjjoling@gmail.com>
2026-02-04 19:13:06 -0600

    Added hamburger menu without hamburger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
+    if (window.innerWidth <= 1400) {
+        return;
+    }
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         this.document.getElementById("navbar-bg").style.top = "-20px";
